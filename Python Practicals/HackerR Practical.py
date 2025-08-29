fc8e0df4088e9c4b4a018f77252dd03c7da7f1c3
Shaunak Pandya <176204036+SRPpandya2004@users.noreply.github.com>
2025-08-29 23:44:21 +0530

    Update HackerR Practical.py

--- Python Practicals/HackerR Practical.py
+++ Python Practicals/HackerR Practical.py
@@ -2,7 +2,6 @@
 
 # Note: Your phone book should be a Dictionary/Map/HashMap data structure.
 
-
 # Input Format
 
 # The first line contains an integer, , denoting the number of entries in the phone book.
